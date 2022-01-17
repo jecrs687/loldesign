435307fcbfa9750bb95fc6b639f55af8c0da1f7b
Emanuel Cascone <emanuelcascone@gmail.com>
2022-01-17 19:09:28 +0100

    gh-pages

--- src/redux/ActionTypes.ts
+++
@@ -1,6 +0,0 @@
-export const SELECT_FIRST_NUMBER: string = "SELECT_FIRST_NUMBER"
-export const SELECT_SECOND_NUMBER: string = "SELECT_SECOND_NUMBER"
-export const SELECT_PLAN: string = "SELECT_PLAN"
-export const MINUTES: string = "MINUTES"
-
-export const INVERT: string = "INVERT"
--- src/shared/nums.ts
+++
@@ -1,3 +0,0 @@
-
-export const First: Array<string> = ["011"]
-export const Second: Array<string> = ["016", "017", "018"]
